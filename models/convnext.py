b8e85fd5bd52f7ba1f5c66bd444cc181a332f9fc
Adheena-TM <adheenam43@gmail.com>
2026-05-19 11:47:51 +0530

    Added SE attention to ConvNeXt blocks

--- models/convnext.py
+++ models/convnext.py
@@ -12,6 +12,30 @@ import torch.nn.functional as F
 from timm.models.layers import trunc_normal_, DropPath
 from timm.models.registry import register_model
 
+class SEBlock(nn.Module):
+
+    def __init__(self, channels, reduction=16):
+
+        super(SEBlock, self).__init__()
+
+        self.pool = nn.AdaptiveAvgPool2d(1)
+
+        self.fc = nn.Sequential(
+            nn.Linear(channels, channels // reduction),
+            nn.ReLU(inplace=True),
+            nn.Linear(channels // reduction, channels),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+
+        b, c, h, w = x.size()
+
+        y = self.pool(x).view(b, c)
+
+        y = self.fc(y).view(b, c, 1, 1)
+
+        return x * y
 class Block(nn.Module):
     r""" ConvNeXt Block. There are two equivalent implementations:
     (1) DwConv -> LayerNorm (channels_first) -> 1x1 Conv -> GELU -> 1x1 Conv; all in (N, C, H, W)
@@ -28,6 +52,7 @@ class Block(nn.Module):
         self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim) # depthwise conv
         self.norm = LayerNorm(dim, eps=1e-6)
         self.pwconv1 = nn.Linear(dim, 4 * dim) # pointwise/1x1 convs, implemented with linear layers
+        self.se = SEBlock(dim)
         self.act = nn.GELU()
         self.pwconv2 = nn.Linear(4 * dim, dim)
         self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)), 
@@ -42,6 +67,9 @@ class Block(nn.Module):
         x = self.pwconv1(x)
         x = self.act(x)
         x = self.pwconv2(x)
+        x = x.permute(0, 3, 1, 2)
+        x = self.se(x)
+        x = x.permute(0, 2, 3, 1)
         if self.gamma is not None:
             x = self.gamma * x
         x = x.permute(0, 3, 1, 2) # (N, H, W, C) -> (N, C, H, W)
